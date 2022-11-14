eb5da7f4f139e7a292a5f19fe990b08f1b9a1ce1
Youmee Lee <91410713+ymStudyLog@users.noreply.github.com>
2022-11-14 14:18:20 +0900

    modify : 주석
    - 주석 추가 : 참고해서 1순위로 리팩토링 & 수정 예정

--- src/hooks/useReactQuery.ts
+++ src/hooks/useReactQuery.ts
@@ -2,16 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getReservationData } from "../api/api";
 import { ReservationType } from "../types";
 
+/*useReactQuery가 하는 일 => reservations 데이터만 다루기 때문에 useReservations로 바꿔야될듯
+*1. db의 reservations 데이터를 가져온다.
+*2. 가져온 reservations 데이터를 Context API로 전역 상태에 저장한다. => Hotel 페이지에서 이 전역 상태를 가져다가 searchQuery를 만들어서 HotelList에 props로 주면 infiniteScroll에서 이것을 가지고  fetching함 
+*3. 예약 버튼을 누르면 reservations 데이터를 refetching 할 수 있도록 react-query를 적용한다.
+*/
 const useReactQuery = () => {
   const reservations = useQuery(["getReservation"], () =>
     getReservationData<ReservationType[]>("")
   );
-
-  /*
-   queryClient -> index.tsx에서 하지 않고 App단위에서 다시 해보고 -> App 컴포넌트 단위에서 queryClient를 생성하면 이때 이미 fetching을 시도하게 되니
-   option을 windowFocus : false로만 설정하기 
-   //TODO index.tsx에 있는 queryClient app으로 가져오고 useReactQuery를 삭제하기 => hotel 페이지에서는 key로 언제든 불러와서 refetching 하면됨
-  */
+  
   return {
     reservations: reservations.data === undefined ? [] : reservations.data,
   };
