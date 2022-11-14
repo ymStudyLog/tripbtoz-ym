168fc4b62c94cf97b2f978e142615a55824a180c
Youmee Lee <91410713+ymStudyLog@users.noreply.github.com>
2022-11-14 14:08:40 +0900

    modify : export 수정
    - export 수정

--- src/layout/Background.tsx
+++ src/layout/Background.tsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
-const Background = styled.div`
+export const Background = styled.div`
   min-height: 100vh;
   background-image: url("./images/background.jpg");
   background-size: 100% 100vh;
   background-repeat: repeat-y;
 `;
-
-export default Background;
