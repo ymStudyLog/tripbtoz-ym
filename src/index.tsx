3ed5fca26c63f65c0e5ec4851b1a16c004aea389
Youmee Lee <91410713+ymStudyLog@users.noreply.github.com>
2022-11-14 14:12:15 +0900

    modify :  react-query 적용 제거
    - App 컴포넌트에서 적용할 예정

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./styles/GlobalStyles";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const myQueryClient = new QueryClient({
-  defaultOptions:{
-    queries: { refetchOnWindowFocus: false},
-  },
-});
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={myQueryClient}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+     <GlobalStyles />
+     <BrowserRouter>
+       <App />
+     </BrowserRouter>
   </React.StrictMode>
 );
