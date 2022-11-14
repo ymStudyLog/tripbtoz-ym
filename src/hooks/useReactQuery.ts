c21274c786125c7eb9a1433d89e5314e4c2ef9d0
Youmee Lee <91410713+ymStudyLog@users.noreply.github.com>
2022-11-14 13:02:54 +0900

    modify : 주석 추가
    - 수정할 내용 주석 추가

--- src/hooks/useReactQuery.ts
+++ src/hooks/useReactQuery.ts
@@ -7,6 +7,11 @@ const useReactQuery = () => {
     getReservationData<ReservationType[]>("")
   );
 
+  /*
+   queryClient -> index.tsx에서 하지 않고 App단위에서 다시 해보고 -> App 컴포넌트 단위에서 queryClient를 생성하면 이때 이미 fetching을 시도하게 되니
+   option을 windowFocus : false로만 설정하기 
+   //TODO index.tsx에 있는 queryClient app으로 가져오고 useReactQuery를 삭제하기 => hotel 페이지에서는 key로 언제든 불러와서 refetching 하면됨
+  */
   return {
     reservations: reservations.data === undefined ? [] : reservations.data,
   };
