9e71a1930f804267e8aa33ce3d16d6fead02be3f
Youmee Lee <91410713+ymStudyLog@users.noreply.github.com>
2022-11-14 14:17:42 +0900

    modify : queryClient 적용
    - App 컴포넌트에서 queryClient 적용 라우터랑 충돌없는지 확인 요망(이전 index.tsx에서 queryClient Provider>BrowserRouter 지금은 BrowserRouter>queryClient Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Router from "./router/Router";
 import { useLocation } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const App = () => {
+  const myQueryClient = new QueryClient({
+    defaultOptions:{
+        queries: { refetchOnWindowFocus: false },
+    },
+  });
+  
   const location = useLocation().pathname;
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  return <Router />;
+  return(
+    <QueryClientProvider client={myQueryClient}>
+      <Router />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
