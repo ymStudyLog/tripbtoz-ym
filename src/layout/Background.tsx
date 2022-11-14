47487c4dfe94d8e6ef6d489e015e0cddc12451b7
Youmee Lee <91410713+ymStudyLog@users.noreply.github.com>
2022-11-14 13:57:50 +0900

    feat : DefaultLayout -> Background 컴포넌트로 변경
    - DefaultLayout -> Background 컴포넌트로 변경

---
+++ src/layout/Background.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Background = styled.div`
+  min-height: 100vh;
+  background-image: url("./images/background.jpg");
+  background-size: 100% 100vh;
+  background-repeat: repeat-y;
+`;
+
+export default Background;
